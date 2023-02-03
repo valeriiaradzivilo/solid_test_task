0a6dd1e884792d2e96ce71dd4331cbed2d6b6738
valeriiaradzivilo <v.radzivilo.ip14@kpi.ua>
2023-02-03 23:43:38 +0200

    rewritten test file as it was made for base counter program

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,8 @@ void main() {
     await tester.pumpWidget(const Main());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('Hi there'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
